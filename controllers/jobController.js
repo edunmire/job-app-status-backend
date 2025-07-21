10b7b84fbce74f8ef9a8820a4487062d0e830e6b
Max Kristian Dunmire <mdunmire@uci.edu>
2025-07-21 03:03:17 -0700

    Added atlas database and connection logic

---
+++ controllers/jobController.js
@@ -0,0 +1,13 @@
+
+
+export async function getJob(req, res) {
+
+}
+
+export async function createJob(req, res) {
+
+}
+
+export async function deleteJob(req, res) {
+    
+}
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 
 app.use(express.json())
 
 
-
-app.listen(process.env.PORT, () => console.log("listening on port 4000"))
+mongoose.connect(process.env.DB_URI).then(
+    app.listen(process.env.PORT, () => console.log(`connected & listening on port ${process.env.PORT}`))
+)
